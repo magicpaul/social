a5e4ae11af2467b2d4adaeb1438f858d879e6a42
magicpaul <itspaulgrant@gmail.com>
2014-05-05 11:02:50 +0100

    tiny little changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140419143337) do
+ActiveRecord::Schema.define(:version => 20140505092222) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
@@ -107,8 +107,8 @@ ActiveRecord::Schema.define(:version => 20140419143337) do
 
   create_table "quizzes", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   create_table "read_marks", :force => true do |t|
